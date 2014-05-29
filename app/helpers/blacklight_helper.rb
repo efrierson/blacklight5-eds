4a4abee950103c2cb62c90412e87ba81ed157a87
Eric Frierson <efrierson@ebscohost.com>
2014-05-29 14:28:24 -0500

    Trying

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -1,7 +1,11 @@
 module BlacklightHelper
   include Blacklight::BlacklightHelperBehavior
 
-  def application_name
-    "Bestest University Search"
+  def render_search_bar
+    if controller_name == "articles"
+      render :partial=>'articles/search_form'
+    else
+      render :partial=>'catalog/search_form'
+    end
   end
 end
